27fb7ba81a5857cec3d1dc87f6bfefd09de39f95
Wieland Gmeiner <wieland.gmeiner@gmail.com>
2020-04-10 14:29:53 +0200

    Make initialization static

--- src/main/java/util/S256Util.java
+++ src/main/java/util/S256Util.java
@@ -12,27 +12,30 @@ import java.math.BigInteger;
  */
 public final class S256Util {
     // prime
-    public static BigInteger        P
-            = BigInteger.valueOf(2).pow(256)
-            .subtract(BigInteger.valueOf(2).pow(32)).subtract(BigInteger.valueOf(977));
+    public static BigInteger        P;
     // order of the curve
-    public static BigInteger        N
-            = new BigInteger("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16);
+    public static BigInteger        N;
     // secp256k1 parameter
-    public static S256FieldElement  A
-            = new S256FieldElement(BigInteger.ZERO);
+    public static S256FieldElement  A;
     // secp256k1 parameter
-    public static S256FieldElement  B
-            = new S256FieldElement(BigInteger.valueOf(7));
+    public static S256FieldElement  B;
     // generator point
-    public static S256Point         G
-            = new S256Point(
-            new S256FieldElement(
-                    new BigInteger("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16)),
-            new S256FieldElement(
-                    new BigInteger("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16))
-    );
+    public static S256Point         G;
     // point at infinity
-    public static S256Point         INFINITY
-            = new S256Point(null, null);
+    public static S256Point         INFINITY;
+
+    static {
+        P = BigInteger.valueOf(2).pow(256).subtract(BigInteger.valueOf(2).pow(32)).subtract(BigInteger.valueOf(977));
+        N = new BigInteger("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16);
+        A = new S256FieldElement(BigInteger.ZERO);
+        B = new S256FieldElement(BigInteger.valueOf(7));
+        G = new S256Point(
+                new S256FieldElement(
+                    new BigInteger("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16)),
+                new S256FieldElement(
+                    new BigInteger("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16)));
+        INFINITY = new S256Point(null, null);
+    }
+
+
 }
